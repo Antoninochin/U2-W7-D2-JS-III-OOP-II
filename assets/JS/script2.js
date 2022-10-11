603ef505e08082869b90349c5c3174d4cd9054d7
Antoninochin <antonino_chindemi@virgilio.it>
2022-10-11 17:06:39 +0200

    js

--- assets/JS/script2.js
+++ assets/JS/script2.js
@@ -1,10 +1,9 @@
 class Page {
 
-    constructor(){
-    this.items = []
+    constructor(name){
+    this.items = [name]
     this.pageSize = []
     }
-pageList()
 
 }
 
@@ -20,3 +19,14 @@ let lists = [
     new Page('bicchieri'),
     new Page('thè')
 ]
+
+function add(event){
+    let list = document.getElementById('list-1')
+        let ul = document.createElement('ul')
+        list.appendChild('ul')
+    for(let i = 0; i < lists.length; i++){
+        let li = document.createElement('li')
+        ul.appendChild('li')
+        li.innerText = lists.name[i]
+    }
+}
